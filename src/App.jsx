4b520015cfb235121c2539e7ad8ab6727ed56d64
rayan472 <rayanaly000@gmial.com>
2024-08-27 23:40:07 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function App() {
+  return (
+    <div>
+        <div class="container py-4 px-3 mx-auto">
+      <h1>Hello, Bootstrap and Vite!</h1>
+      <button class="btn btn-primary ">Primary button</button>
+    </div>
+    </div>
+  )
+}
